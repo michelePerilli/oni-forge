797c7b9fb3c49a201a7b8e1e8b2a050c43f4995b
Michele Perilli <micheleperilli.98@libero.it>
2026-03-15 17:46:51 +0100

    build: add PugiXML dependency

--- src/OniForgeApp.cpp
+++ src/OniForgeApp.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <pugixml.hpp>
 
 int main() {
     std::cout << "This is oni forge!" << std::endl;
+    pugi::xml_document doc;
+    std::cout << "PugiXML version: " << PUGIXML_VERSION << " is ready!" << std::endl;
     return 0;
 }
